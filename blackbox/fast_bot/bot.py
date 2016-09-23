7b4034d7d76e3af47a05f1450926696ecc22e0c3
Truc Viet Le <vietexob@gmail.com>
2016-09-23 22:58:17 +0800

    Removed blackbox project

--- blackbox/fast_bot/bot.py
+++
@@ -1,5 +0,0 @@
-import pyximport, numpy
-pyximport.install(setup_args={"include_dirs":numpy.get_include()}, reload_support=True)
-
-import super_fast_bot as sfb 
-sfb.run_bbox()
--- blackbox/fast_bot/interface.pxd
+++
@@ -1,7 +0,0 @@
-cdef:
-	float* c_get_state() 
-	int c_do_action(int action) except -1
-	float c_get_score()
-	int c_get_time()
-	int c_is_level_finished()
-	
